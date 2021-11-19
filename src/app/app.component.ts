70e0af210b89b668484637a2b9e93fd6de9aff9c
rmoyaTI <r.moya@cesariglesias.com.do>
2021-11-19 16:44:03 -0400

    Agregar autenticacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
-import { Product } from './models/product.model';
+
+import { AuthService } from './services/auth.service';
+
+import { UsersService } from './services/users.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,9 +13,18 @@ import { Product } from './models/product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService
+  ){
+
+  }
+
   imgParent = '';
   showImg = true;
-
+  token = '';
+  userMail = '';
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -19,4 +33,17 @@ export class AppComponent {
   toggleImg() {
     this.showImg = !this.showImg;
   }
+
+  createUser(){
+    this.userService.create({
+      name: 'Roland',
+      email: 'roland@mail.com',
+      password: '12345'
+    })
+    .subscribe(rta => {
+      console.log(rta);
+    })
+  }
+
+
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,21 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/services/auth.service';
+
+import { UsersService } from 'src/app/services/users.service';
+
+import { Input } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  @Input() userMail: string = '';
 
   activeMenu = false;
+  token = '';
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService
+  ) { }
+
+
 
   ngOnInit(): void {
   }
 
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
+  }
+
+  login(){
+
+    this.authService.login(
+     'roland@mail.com',
+     '12345'
+    ).pipe(
+      switchMap((token) => {
+        this.token = token.access_token
+        return this.authService.profile(token.access_token);
+      })
+    )
+    .subscribe(rta => {
+      this.userMail = rta.email;
+    });
+
+  }
+
+  getProfile(){
+    this.authService.profile(this.token)
+    .subscribe(profile => {
+      this.userMail = profile.email;
+      console.log(profile)
+    });
   }
 
 }
