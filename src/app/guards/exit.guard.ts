61374f89519c2efe7339b0cfdc4e7e6c644ca78c
rmoyaTI <r.moya@cesariglesias.com.do>
2021-11-25 12:07:09 -0400

    CanDeactivate guard

--- src/app/guards/exit.guard.ts
+++ src/app/guards/exit.guard.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
+export interface OnExit{
+  onExit: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ExitGuard implements CanDeactivate<unknown> {
   canDeactivate(
-    component: unknown,
+    component: OnExit,
     currentRoute: ActivatedRouteSnapshot,
     currentState: RouterStateSnapshot,
     nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const rta = confirm('Estas seguro que desea salir?');
-    return rta ? true : false;
+
+
+    return component.onExit ? component.onExit() : true;
   }
 
 }
